0cf4aafb7c37bf8b77afc285750bb1f09dd2b923
sharmaiiitb <ind.pankajsharma@gmail.com>
2022-02-24 21:53:05 +0530

    adding data processing program for machine learning

--- data_processing.py
+++ data_processing.py
@@ -1,8 +1,17 @@
+from this import d
+
+
 print("adding all the data processing code related fraud detection")
 a = 6
 b = 7
-def sum(a,b):
-    c = a+b
-    return a
+def multiplication(a,b):
+    c = a*b
+    return c
 
-    print('added a new function sum')
+    print('added a new function multiplication')
+
+    def divivsion(a,b)
+    d = a/b
+    return d
+    print('division of two numbers')
+    
